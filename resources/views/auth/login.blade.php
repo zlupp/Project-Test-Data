4554bae3ee99fb026958ed8f4a1fd7f271ed8de4
razaqslm <razaqslmslm@gmail.com>
2019-11-25 20:44:41 +0700

    comit kedua

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,7 +11,7 @@
   <meta name="keywords"
     content="materialize, admin template, dashboard template, flat admin template, responsive admin template, eCommerce dashboard, analytic dashboard">
   <meta name="author" content="ThemeSelect">
-  <title>User Login | Materialize - Material Design Admin Template</title>
+  <title>Login | Test Data</title>
   <link rel="apple-touch-icon" href="assets/images/apple-touch-icon-152x152.png">
   <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon-32x32.png">
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
